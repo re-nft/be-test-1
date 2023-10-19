a713471ed5eaec8c15abdda13f3fe739504c6826
Nazariy Vavryk <hi@naz.ooo>
2023-10-19 17:43:48 +0100

    chore: touch up processor

--- indexer/src/processor.ts
+++ indexer/src/processor.ts
@@ -1,38 +1,36 @@
-import {lookupArchive} from '@subsquid/archive-registry'
+import { lookupArchive } from "@subsquid/archive-registry";
 import {
-    BlockHeader,
-    DataHandlerContext,
-    EvmBatchProcessor,
-    EvmBatchProcessorFields,
-    Log as _Log,
-    Transaction as _Transaction,
-} from '@subsquid/evm-processor'
+  BlockHeader,
+  DataHandlerContext,
+  EvmBatchProcessor,
+  EvmBatchProcessorFields,
+  Log as _Log,
+  Transaction as _Transaction,
+} from "@subsquid/evm-processor";
 
 export const processor = new EvmBatchProcessor()
-    .setDataSource({
-        // Change the Archive endpoints for run the squid
-        // against the other EVM networks
-        // For a full list of supported networks and config options
-        // see https://docs.subsquid.io/evm-indexing/
-        archive: lookupArchive('polygon'),
-    })
-    .setFinalityConfirmation(75)
-    .setFields({
-        transaction: {
-            from: true,
-            value: true,
-            hash: true,
-        },
-    })
-    .setBlockRange({
-        from: 6_000_000,
-    })
-    .addTransaction({
-        to: ['0x0000000000000000000000000000000000000000'],
-    })
+  .setDataSource({
+    // Change the Archive endpoints for run the squid
+    // against the other EVM networks
+    // For a full list of supported networks and config options
+    // see https://docs.subsquid.io/evm-indexing/
+    archive: lookupArchive("polygon"),
+  })
+  .setFinalityConfirmation(75)
+  .setFields({
+    transaction: {
+      from: true,
+      value: true,
+      hash: true,
+    },
+  })
+  .setBlockRange({
+    from: 36825974, // this is valid renft contract creation data
+    // ! do not change it
+  });
 
-export type Fields = EvmBatchProcessorFields<typeof processor>
-export type Block = BlockHeader<Fields>
-export type Log = _Log<Fields>
-export type Transaction = _Transaction<Fields>
-export type ProcessorContext<Store> = DataHandlerContext<Store, Fields>
+export type Fields = EvmBatchProcessorFields<typeof processor>;
+export type Block = BlockHeader<Fields>;
+export type Log = _Log<Fields>;
+export type Transaction = _Transaction<Fields>;
+export type ProcessorContext<Store> = DataHandlerContext<Store, Fields>;
